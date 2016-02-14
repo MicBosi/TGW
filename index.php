b03d9a1c4d8569bcd7f6c71064964db015bdd867
Michele <hello@michelebosi.com>
2016-02-14 14:37:17 +0000

    update

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
-  require("config.php");
-  require("include/index.php");
+	/*
+	TODO:
+	- unificazione font styles
+	- update title font
+	- shrink "pre" tablature text
+	- refactor this index.php
+	- update GA code
+	- update AdSense code
+	*/
+	require("config.php");
+	require("include/index.php");
 ?>
